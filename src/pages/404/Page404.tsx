b22579d8bbc315cc869845a5332f0a0a90e4c3d6
X6uddy <sashavolkov_03@mail.ru>
2024-05-04 12:56:33 +0400

    minor changes

--- src/pages/404/Page404.tsx
+++ src/pages/404/Page404.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Page404: FC = () => {
     return (
-        <div className="">Page not found</div>
+        <div style={{color: '#fff', padding: '40px', fontSize: '30px'}}>Page not found</div>
     )
 }
 export default Page404;
