fc07601f7db19c25b3ee5ca157317a46555a9918
X6uddy <sashavolkov_03@mail.ru>
2024-05-04 12:56:16 +0400

    adding interface Order

--- src/models/response/OrderResponse.ts
+++ src/models/response/OrderResponse.ts
@@ -1,12 +1,12 @@
 import { IUser } from "../IUser";
 
-export type orderStatus = 'not confirmed' | 'confirmed' | 'assembling' | 'in delivery' | 'delivered';
+export type OrderStatus = 'not confirmed' | 'confirmed' | 'assembling' | 'in delivery' | 'delivered';
 
 export interface OrderResponse {
     orderId: string,
     userId: string,
     purchasedAutoParts: string[];
     orderTime: string;
-    status: orderStatus;
+    status: OrderStatus;
     user?: IUser;
 }
