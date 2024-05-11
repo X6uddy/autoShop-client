f5b9e27cb6c74cd875fc72d09a1a3bd790378c54
X6uddy <sashavolkov_03@mail.ru>
2024-05-12 00:39:21 +0400

    adding useEffect deps

--- src/pages/orders/OrderList/OrderList.tsx
+++ src/pages/orders/OrderList/OrderList.tsx
@@ -31,7 +31,7 @@ export function OrderList() {
             }
         }
         fetchOrdersById();
-    }, [])
+    }, [clientId])
     console.log(orders)
     if(orders.length < 1) {
         return (
