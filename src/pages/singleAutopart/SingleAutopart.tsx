fe2803dbcfd8ced2b6999592b4667e43715cf819
X6uddy <sashavolkov_03@mail.ru>
2024-05-31 20:29:02 +0400

    adding functionally for add item to basket

--- src/pages/singleAutopart/SingleAutopart.tsx
+++ src/pages/singleAutopart/SingleAutopart.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import toast from "react-hot-toast";
 import { useParams } from "react-router";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
@@ -8,17 +8,20 @@ import AutopartService from "../../services/AutopartsService";
 import styles from './SingleAutopart.module.scss';
 import { Button } from "@mui/material";
 import { observer } from "mobx-react-lite";
+import { CheckSquare2Icon } from "lucide-react";
+import { Context } from "../../main";
 
 export function SingleAutopart() {
     const {autopartId} = useParams();
+    const {basket} = useContext(Context);
     const [autopart, setAutopart] = useState<AutopartsResponse[]>([])
-    const [loading, setLoading] = useState(true)
-    console.log('auto', autopartId)
+    const [loading, setLoading] = useState(true);
+    const [basketStatus, setBasketStatus] = useState<boolean>(false)
+    const [quantityOnBasket, setQuantityOnBasket] = useState<number>(0);
     useEffect(() => {
         const fetchAutopart = async () => {
             try {
                 const response = await AutopartService.getAutopartById(autopartId!);
-                console.log(response.data)
                 setAutopart(response.data)
                 toast.success('Заказы пользователя найдены')
             } catch (e) {
@@ -30,7 +33,21 @@ export function SingleAutopart() {
             }
         }
         fetchAutopart();
-    }, [autopartId])
+        const existingItemIndex = basket.basketItems.findIndex(item => item.item.autopartId === autopartId)
+        if(basket.basketItems.find(item => item.item.autopartId === autopartId)){
+            if (existingItemIndex !== -1) {
+                setQuantityOnBasket(basket.basketItems[existingItemIndex].quantity)
+            }
+            setBasketStatus(true)
+        }else{
+            setBasketStatus(false)
+        }
+    }, [basket.basketItems, basketStatus, quantityOnBasket])
+    function addItemToBasket (item: AutopartsResponse) {
+        basket.addItem(item);
+        setBasketStatus(true)
+        setQuantityOnBasket(quantityOnBasket => quantityOnBasket + 1)
+    }
 
     if(loading) {
         return (
@@ -50,12 +67,24 @@ export function SingleAutopart() {
                 <div><span>Вес запчасти: </span>{autopart[0].weight} кг</div>
                 <div><span>Наличие: </span>{autopart[0].quantitOnHand} штук</div>
                 <div><span>Совместимость: </span>{autopart[0].compatability.map((item,i) => (
-                    <div style={{marginLeft: '10px'}}><span>{i+1}. </span>{item}<br/></div>
+                    <div style={{marginLeft: '10px'}} key={i}><span>{i+1}. </span>{item}<br/></div>
                 ))}</div>
                 <div><span>Описание: </span>{autopart[0].description}</div>
-                <Button variant="contained">
-                    Добавить в корзину <ShoppingBasketIcon/>
-                </Button>
+                {basketStatus ? 
+                    <Button 
+                        variant="contained"
+                        onClick={() => addItemToBasket(autopart[0])}
+                    >
+                        Добавить в корзину
+                        <p style={{margin: '0', marginLeft: '10px', marginRight: '10px'}}>{quantityOnBasket}</p>
+                        {basketStatus ? <CheckSquare2Icon/> : <ShoppingBasketIcon/>}
+                    </Button>
+                    :
+                    <Button variant="contained" onClick={() => addItemToBasket(autopart[0])}>
+                        Добавить в корзину
+                        {basketStatus ? <CheckSquare2Icon/> : <ShoppingBasketIcon/>}
+                    </Button>
+                }
             </div>
         </div>
     )
