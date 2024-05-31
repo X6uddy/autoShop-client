b104b0cf691ff5e14ef05ab2bab0a68d1748292e
X6uddy <sashavolkov_03@mail.ru>
2024-05-31 20:29:31 +0400

    adding types for autoparts array

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,6 +1,7 @@
 import $api from "../http";
 import { AxiosResponse } from "axios";
 import { OrderResponse, OrderStatus } from "../models/response/OrderResponse";
+import { ExpandAutopartsResponse } from "../store/basket";
 
 export default class OrderService {
     static async getAllOrders(): Promise<AxiosResponse<OrderResponse[]>> {
@@ -9,8 +10,8 @@ export default class OrderService {
     static async getOrdersById(userId: string): Promise<AxiosResponse<OrderResponse[]>> {
         return $api.post<OrderResponse[]>('/orders/:id', {userId});
     }
-    static async makeNewOrder(login: string, autoparts: string[]): Promise<AxiosResponse<OrderResponse[]>> {
-        return $api.post<OrderResponse[]>('/orders/new', {autoparts, login});
+    static async makeNewOrder(userId: string, autoparts: ExpandAutopartsResponse[]): Promise<AxiosResponse<OrderResponse[]>> {
+        return $api.post<OrderResponse[]>('/orders/new', {autoparts, userId});
     }
     static async statusСhange(orderId: string, newStatus: OrderStatus): Promise<AxiosResponse<OrderResponse[]>> {
         return $api.patch<OrderResponse[]>('/orders/changeStatus', {orderId, newStatus});
