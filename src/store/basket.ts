b6182dab5e3e778fa576a061c4e4ea12b77a2dd9
X6uddy <sashavolkov_03@mail.ru>
2024-05-31 20:29:45 +0400

    adding local stroage

--- src/store/basket.ts
+++ src/store/basket.ts
@@ -1,53 +1,72 @@
-import {IUser} from "../models/IUser";
-import {makeAutoObservable} from "mobx";
-import AuthService from "../services/AuthService";
-import axios from 'axios';
-import {AuthResponse} from "../models/response/AuthResponse";
-import {API_URL} from "../http";
-import toast from "react-hot-toast";
+import { makeAutoObservable } from "mobx";
 import { AutopartsResponse } from "../models/response/AutopartsResponse";
+import OrderService from "../services/OrderService";
+import toast from "react-hot-toast";
 
 export interface ExpandAutopartsResponse {
     item: AutopartsResponse;
     quantity: number;
 }
-export class Basket {
+
+const BASKET_STORAGE_KEY = "basketItems";
+
+export default class Basket {
     basketItems = [] as ExpandAutopartsResponse[];
 
     constructor() {
         makeAutoObservable(this);
+        this.loadBasket();
+    }
+
+    saveBasket() {
+        localStorage.setItem(BASKET_STORAGE_KEY, JSON.stringify(this.basketItems));
+    }
+
+    loadBasket() {
+        const savedBasket = localStorage.getItem(BASKET_STORAGE_KEY);
+        if (savedBasket) {
+            this.basketItems = JSON.parse(savedBasket);
+        }
     }
 
     addItem(item: AutopartsResponse) {
-      const existingItemIndex = this.basketItems.findIndex(cartItem => cartItem.item.autopartId === item.autopartId);
-      if (existingItemIndex !== -1) {
-        // Если товар уже есть в корзине, увеличиваем его количество
-        this.basketItems[existingItemIndex].quantity += 1;
-      } else {
-        // Иначе добавляем новый товар в корзину
-        this.basketItems.push({
-            quantity: 1,
-            item: item
-        });
-      }
+        const existingItemIndex = this.basketItems.findIndex(cartItem => cartItem.item.autopartId === item.autopartId);
+        if (existingItemIndex !== -1) {
+            this.basketItems[existingItemIndex].quantity += 1;
+        } else {
+            this.basketItems.push({
+                quantity: 1,
+                item: item
+            });
+        }
+        this.saveBasket();
     }
-  
+
     removeItem(id: string) {
-      const itemIndex = this.basketItems.findIndex(cartItem => cartItem.item.autopartId === id);
-      if (itemIndex !== -1) {
-        this.basketItems.splice(itemIndex, 1);
-      }
+        const itemIndex = this.basketItems.findIndex(cartItem => cartItem.item.autopartId === id);
+        if (itemIndex !== -1) {
+            if (this.basketItems[itemIndex].quantity === 1) {
+                this.basketItems.splice(itemIndex, 1);
+            } else {
+                this.basketItems[itemIndex].quantity -= 1;
+            }
+            this.saveBasket();
+        }
     }
-  
-    get itemCount() {
-      return this.basketItems.reduce((total, item) => total + item.quantity, 0);
+
+    async makeNewOrder(login: string, autoparts: ExpandAutopartsResponse[]) {
+      console.log(login, autoparts)
+      try {
+        await OrderService.makeNewOrder(login, autoparts);
+        toast.success('Заказ успешно отправлен')
+      } catch (e) {
+        console.log(e);
+        // @ts-ignore   
+        toast.error(`${e.response?.data?.message}`);
     }
-  
-    get totalPrice() {
-      return this.basketItems.reduce((total, item) => total + (item.item.price * item.quantity), 0);
+    }
+
+    getTotalPrice() {
+        return this.basketItems.reduce((total, item) => total + (item.item.price * item.quantity), 0);
     }
 }
-  
-const cartStore = new Basket();
-
-export default cartStore;
